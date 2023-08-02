1ceb7d34b5b98539df4407fe51d2dab6766f7e31
Abdullahhafeez-Github <135792135+Abdullahhafeez-Github@users.noreply.github.com>
2023-08-02 21:27:38 +0500

    Update config.js

--- config.js
+++ config.js
@@ -47,10 +47,10 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'ᴛʜᴇ ɢᴜʀᴜ-ʙᴏᴛ'
+global.botname = 'Cyber Warrior Abdullah Hafeez'
 global.premium = 'true'
-global.packname = 'GURU┃ᴮᴼᵀ' 
-global.author = '@Asliguru' 
+global.packname = 'Stiker' 
+global.author = '@Cyber-Warrior-Abdullah-Hafeez' 
 global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/asli_guru69\n' 
 global.dygp = 'https://chat.whatsapp.com/BibVE6tUICp3qOm2a1Ur8f'
 global.fgsc = 'https://github.com/Guru322/GURU-BOT-V1' 
